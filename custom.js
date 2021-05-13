db89f892da9931fcfe96604fd369cb8356a8c3cb
Sohan1975 <84146041+Sohan1975@users.noreply.github.com>
2021-05-13 09:53:26 -0700

    added html and css files

---
+++ custom.js
@@ -0,0 +1,15 @@
+       var typed = new Typed('.text', {
+          strings: ["Web Designer.", "Web Devoloper."],
+          typeSpeed: 30,
+            loop:true,
+            typeSpeed: 50,
+            backSpeed: 50
+            
+        });
+    jQuery(document).ready(function($) {
+            $('.counter').counterUp({
+                delay: 5,
+                time: 1000
+            });
+        });
+
